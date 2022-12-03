ab68c2e62e8b1578a3266f91195bb1302b881dc2
yasi <zahrafarrokhi2017@gmail.com>
2022-12-03 13:27:22 +0330

    Add requirements for backend

--- todo/backend/todo/settings.py
+++ todo/backend/todo/settings.py
@@ -138,7 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOW_ALL_ORIGINS = True
