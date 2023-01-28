69779cd50299e07331d0909231a0a834242178b5
yasi <zahrafarrokhi2017@gmail.com>
2023-01-28 16:43:28 +0330

    Create nested message field

--- 04 - Chat App/backend/chat/admin.py
+++ 04 - Chat App/backend/chat/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from chat.models import Chat
+from chat.models import Chat, Message
+
+
+class MessageInlineAdmin(admin.TabularInline):
+    model = Message
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('-created_at', 'user',)
+    fields = ('text', 'created_at', 'updated_at', 'user')
 
 
 class ChatAdmin(admin.ModelAdmin):
@@ -9,6 +16,8 @@ class ChatAdmin(admin.ModelAdmin):
     list_display = ('name', )
     ordering = ('-created_at', 'name',)
     fields = ('name', 'created_at', 'updated_at', 'user')
+
+    inlines = [MessageInlineAdmin]
 
 
 admin.site.register(Chat, ChatAdmin)
