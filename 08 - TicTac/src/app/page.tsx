9876f81ef8d65c52d1d9f20612293824b13622f9
Yasi <zahrafarrokhi2017@gmail.com>
2023-08-02 14:57:02 +0330

    remove borders

--- 08 - TicTac/src/app/page.tsx
+++ 08 - TicTac/src/app/page.tsx
@@ -5,45 +5,34 @@ import { IndexType, NumberLiteralType } from "typescript";
 
 export default function Home() {
   const [board, setBoard] = useState([
-    ["", "", ""],
+    ["", "", ""],// ["", "", ""]=>item ,"" =>cell
     ["", "", ""],
     ["", "", ""],
   ]);
-  // ["", "", "", "", "", "", "", "", "", ]
-  // ["x", "", "", "", "", "", "", "", "", ]
+ 
   const [turn, setTurn] = useState("x");
 
   const ninetabe = (row: number, col: number) => {
     let array = board;
 
-    array[row][col] = turn;
-    setBoard(array);
-    // if (index == 0){
-    //   array[0] = turn
-    //   setBoard(array);
-    // }
-    // else if(index==1){
-    //   array[1] = turn
-    //   setBoard(array);
-    // }
-
-    // else if(index==2){
-    //   array[2] = turn
-    //   setBoard(array);
-    // }
-    if (turn === "x") {
-      setTurn("o");
-    } else setTurn("x");
+    if (board[row][col] === ""){
+      array[row][col] = turn;
+      setBoard(array);
+  
+      if (turn === "x") {
+        setTurn("o");
+      } else setTurn("x");
+    }
   };
   return (
     <main className="flex items-center justify-center h-full">
       <div className="flex flex-col bg-black  text-white p-4 [&>div>button]:w-52 [&>div>button]:h-52 [&>div>button]:text-center [&>div>button]:flex [&>div>button]:items-center  [&>div>button]:justify-center [&>div>button]:text-7xl  rounded-lg">
         {/*board is array and item is array  */}
         {board.map((item,row) => (
-          <div className="flex flex-row border-b border-solid border-white  " key={`row-${row}`}>
+          <div   className={ `flex flex-row ${row===2?"":"border-b border-solid border-white"}`} key={`row-${row}`}>
             {item.map((cell,col) => (
               <button
-                className="border-r border-solid border-white"
+                className={`${col===2?"":"border-r border-solid border-white"}`}
                 onClick={() => ninetabe(row, col)}
                 key={`col-${col}`}
               >
